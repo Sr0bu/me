339af9714bda68fba22da1bb6f261074b9416b5a
Sr0bu <sebastianrobu@yahoo.com>
2025-04-09 21:47:52 +0200

    optimized

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import {buttonVariants} from "@/components/ui/button";
 
 export default function Home() {
     return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import {Button, buttonVariants} from "@/components/ui/button";
+import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
-import {ModeToggle} from "@/components/theme/themebutton";
 
 
 export default function Home() {
