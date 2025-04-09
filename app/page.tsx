a82d55bc3a8f5b6ea180d8c8fd9382756fcdb143
Sr0bu <sebastianrobu@yahoo.com>
2025-04-09 22:33:26 +0200

    SpeedInsights

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export default function Home() {
     return (
         <>
+            <SpeedInsights />
             <div className="p-4">
                 <Link href={"./about"} className={buttonVariants({variant: "secondary"})}>About</Link>
             </div>
