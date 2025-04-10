90769bf96da0c69b6013be17c1beae90257f315a
Sr0bu <sebastianrobu@yahoo.com>
2025-04-10 17:59:16 +0200

    fixed comp

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -26,7 +26,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             return res.status(200).json(user) // Erfolgreiche Antwort mit dem gespeicherten Benutzer
         } catch (error) {
             console.error('Error creating user:', error)
-            return res.status(500).json({ error: 'Failed to create user', details: error.message })
         }
     } else {
         res.setHeader('Allow', ['POST'])
