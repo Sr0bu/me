793ce8f225ebea5ab82078fc8d62c2b0cf3d5f70
Sr0bu <sebastianrobu@yahoo.com>
2025-04-25 10:49:40 +0200

    fixed #16 (Broken Login)

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -11,16 +11,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const { identifier, password } = req.body
 
-    if (!identifier || !password) {
-        return res.status(400).json({ error: 'Missing identifier or password' })
-    }
-
-    // Email oder Username erkennen
-    const isEmail = identifier.includes('@')
-
-    const user = isEmail
-        ? await prisma.user.findUnique({ where: { email: identifier } })
-        : await prisma.user.findFirst({ where: { username: identifier } })
+    const user = await prisma.user.findFirst({
+        where: {
+            OR: [{ email: identifier }, { username: identifier }],
+        },
+    })
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
         return res.status(401).json({ error: 'Invalid credentials' })
@@ -32,10 +27,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         { expiresIn: '30d' }
     )
 
+
     res.setHeader(
         'Set-Cookie',
         `token=${token}; HttpOnly; Path=/; Max-Age=3600; SameSite=Lax`
     )
 
-    return res.status(200).json({ message: 'Login successful', userId: user.id })
+    return res.status(200).json({ message: 'Login successful' })
 }
